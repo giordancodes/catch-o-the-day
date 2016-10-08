3e0086ff8dcc60984f3cae95963d108376c97aec
Giordan Battaglin <gio@giordan.ca>
2016-10-08 11:25:09 -0400

    vid 3

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));
