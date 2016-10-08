d80d8c267de971101f187b34a99e32acdf427612
Giordan Battaglin <gio@giordan.ca>
2016-10-08 11:31:09 -0400

    vid 6

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,8 @@ import React from 'react';
 class StorePicker extends React.Component {
 	render(){
 		return(
-			<form className="store-selector">  
+			<form className="store-selector">
+				{/* Comments need to be inside the rendered parent */}
 				<h2>Please Enter A Store</h2>
 				<input type="text" required placeholder="Store Name" />
 				<button type="submit">
