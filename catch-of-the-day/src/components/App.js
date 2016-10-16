ce3c66d760abba1810f1691184af5d2c9b583546
Giordan Battaglin <gio@giordan.ca>
2016-10-16 11:54:27 -0400

    vid 17

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component{
 		
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		this.state = {
 			fishes: {},
 			order: {}
@@ -31,6 +32,14 @@ class App extends React.Component{
 			fishes : sampleFishes
 		})
 	}
+	addToOrder(key){
+		// take a copy of state
+		const order = {...this.state.order};
+		// update or add the new # of fish ordered
+		order[key] = order[key] + 1 || 1;
+		// update our state
+		this.setState({ order });
+	}
 	render(){
 		return(
 			<div className="catch-of-the-day">
@@ -43,7 +52,13 @@ class App extends React.Component{
 							Object
 								.keys(this.state.fishes)
 								.map(key => 
-									<Fish key={key} details ={ this.state.fishes[key] } />)
+									<Fish 
+										key={key} 
+										index={key}
+										details ={ this.state.fishes[key] }
+										addToOrder={this.addToOrder}
+									/>)
+
 					}
 					</ul>
 				</div>
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -5,19 +5,25 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component{
 	render(){
 		// const details = this.props.details;
+		// const index = this.props.index;
 		// these 2 are equivalent with diff syntax
-		const { details } = this.props;
+		const { details, index } = this.props;
+		const isAvailable = details.status === 'available';
+		const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
 		return(
 				<li className="menu-fish">
-				<img src={ details.image } alt={ details.name }/>
-				<h3 className="fish-name">
-					{ details.name }
-					<span className="price">
-						{formatPrice(details.price)}
-					</span>
-				</h3>
-				<p>{ details.desc }</p>
-				<button>Add To Order</button>
+					<img src={ details.image } alt={ details.name }/>
+					<h3 className="fish-name">
+						{ details.name }
+						<span className="price">
+							{formatPrice(details.price)}
+						</span>
+					</h3>
+					<p>{ details.desc }</p>
+					<button
+						onClick={ () => this.props.addToOrder(index) } 
+						disabled={!isAvailable}>{buttonText}
+					</button>
 				</li>
 			)
 	}
